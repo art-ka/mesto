4664857e013d0c26ce2dd6a5f9311647f7fdcdef
Katya Novikova <katy.novikova@gmail.com>
2020-09-04 19:40:07 +0300

    all

--- scripts/index.js
+++ scripts/index.js
@@ -1,9 +1,9 @@
 let popup = document.querySelector('.popup');
-let popupOpenButton = document.querySelector('.profile-info__edit-button');
+let popupOpenButton = document.querySelector('.profile__edit-button');
 let popupCloseButton = document.querySelector('.popup__close');
 
 const popupToggle = function () {
-        popup.classList.toggle('popup__opened');
+        popup.classList.toggle('popup_opened');
     }
 
     popupOpenButton.addEventListener('click', popupToggle);
@@ -16,6 +16,23 @@ const popupToggle = function () {
 let formElement = popup.querySelector('.popup__form'); 
 let submitInput = formElement.querySelector('.popup__submit-button');
 
+function getValue () {
+
+    let title = document.querySelector('.profile__title');
+    let subtitle = document.querySelector('.profile__subtitle');
+
+    let name1 = title.innerHTML;
+    let job1 = subtitle.innerHTML;
+
+    let nInput = formElement.querySelector('.popup__field_name_text');
+    let jInput = formElement.querySelector('.popup__field_job_text');
+
+    nInput.value = name1;
+    jInput.value = job1;
+}
+
+popupOpenButton.addEventListener('click', getValue);
+
 // Обработчик «отправки» формы, хотя пока
 // она никуда отправляться не будет
 function formSubmitHandler (evt) {
@@ -24,21 +41,22 @@ function formSubmitHandler (evt) {
                         // О том, как это делать, расскажем позже.
                         
     // Находим поля формы в DOM
-    let nameInput = formElement.querySelector('.popup__profile-title');
-    let jobInput = formElement.querySelector('.popup__profile-subtitle');
+    let nameInput = formElement.querySelector('.popup__field_name_text');
+    let jobInput = formElement.querySelector('.popup__field_job_text');
 
       // Получите значение полей из свойства value
     let name = nameInput.value;
     let job = jobInput.value; 
 
     // Выберите элементы, куда должны быть вставлены значения полей
-    let profileTitle = document.querySelector('.profile-info__title');
-    let profileSubtitle = document.querySelector('.profile-info__subtitle');
+    let profileTitle = document.querySelector('.profile__title');
+    let profileSubtitle = document.querySelector('.profile__subtitle');
 
     // Вставьте новые значения с помощью textContent
     profileTitle.textContent = name;
     profileSubtitle.textContent = job;
 
+    popupToggle();
 }
 
 // Прикрепляем обработчик к форме:
@@ -46,15 +64,13 @@ function formSubmitHandler (evt) {
 
 formElement.addEventListener('submit', formSubmitHandler); 
 
-submitInput.addEventListener('click', popupToggle);
-
 const popupCloseByClickOnOverlay = (event) => {
 
     if (event.target !== event.currentTarget) {
-    return
+    return;
     }
 
-    popupToggle(event)
+    popupToggle(event);
 }
 
 popup.addEventListener('click', popupCloseByClickOnOverlay);
